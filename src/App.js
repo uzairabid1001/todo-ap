dda49c0c9a5c5d2fbed60a30687ba6cd57f63840
uzair abid <uzairabid1001@gmial.com>
2022-09-26 23:16:28 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,39 +1,87 @@
-import { useState } from 'react';
+
 import './App.css';
+
+import { useState } from 'react'
 
 function App() {
 
-let [txt, setTxt] =  useState(""); 
-let [list,setlist] = useState([])
-let add = ()=>{
-  if (!txt){
-    alert("text is requires")
-    return;   
-  }  
-  list.push(txt);
-  setlist([...list]); 
-}
-let delte = ()=>{
-  setlist.push(0)
-}
+  const [txt, settxt] = useState('')
+  const [list, setlist] = useState([])
+  const [edititem, setedititem] = useState(true)
+  const [Newitem, setNewitem] = useState('')
+  // function to add itme in list 
+
+  function add() {
+    if (!txt) {
+
+    }
+    else if (txt && !edititem) {
+      setlist(list.map((elem) => {
+        if (elem.Token === Newitem) {
+          return { ...elem, Name: txt }
+        }
+        return elem;
+      }))
+      setedititem(true)
+      settxt('')
+      setNewitem(null)
+
+    }
+    else {
+      // list.push(txt)
+      const Items = { Token: new Date().getTime().toString(), Name: txt }
+      setlist([...list, Items]);
+      settxt('')
+    }
+  }
  
+
+  function deleteitem(id) {
+    const updateitem = list.filter((e) => {
+
+      return id !== e.Token;
+
+    })
+    setlist(updateitem)
+  }
+
+  //DeleteAll
+
+  function DeleteAll() {
+   
+    setlist([]);
+  }
   return (
     <div className="App">
       <header className="App-header">
-            <input className='inp'size={30} onChange ={(e)=>{
-                   setTxt(e.target.value);
-                                       
-            }
-          } />
-            <button className='btn' onClick = {add}>Add</button>
-            <button className='btn' onClick = {delte}>delte</button>
-            <ul>
-              {list.map((e,i)=>{
-                 return <li  
-             key = {i} >{e} </li>
-            })}
-            </ul>
-       </header>
+       
+        <div>
+        <input className='inp' size={30} onChange={(e) => {
+          settxt(e.target.value)
+        }} />
+        
+        {
+          edititem ? <button className='btn' onClick={() => add()}> <b>Add item</b></button> : <button onClick={() => add()}> Update item</button>
+        }
+        
+              <button  className='btn' onClick={() => DeleteAll()}><b> Delete All Item </b></button>
+    
+              </div>
+    
+    
+
+        <ul>{list.map((Inputvalue) => {
+
+             return<li key={Inputvalue.Token} >{Inputvalue.Name}
+          <div>
+            <button className='btn-' onClick={() => deleteitem(Inputvalue.Token)}><b>Delete Item</b></button>
+            </div>
+          </li>
+          
+        })}
+        </ul>
+
+      </header>
     </div>
   );
 }
